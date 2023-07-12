8b740797963b0feac82f402c4930b620da9b3e4f
workstation <vladislav.procenko.2002@mail.ru>
2023-07-12 14:41:02 +0700

    Cleaned and corrected project

--- Assets/Scripts/Core/Weapons/Builder/WeaponDataHolder.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WeaponDataHolder
-{
-    public List<WeaponClassDataHolder> WeaponClassDatas { get; set; }
-    public List<WeaponTypeDataHolder> WeaponTypeDatas { get; set; }
-}
--- Assets/Scripts/Core/Weapons/Projectile/DirectedProjectile.cs
+++ Assets/Scripts/Core/Weapons/Projectile/DirectedProjectile.cs
@@ -7,7 +7,6 @@ namespace VampireLike.Core.Weapons
 {
     public class DirectedProjectile : Projectile
     {
-
         public override void Move(float speed, Vector3 point, float flyTime)
         {
 
